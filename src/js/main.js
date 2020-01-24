f4aca5064bc24d69ae444577309a8deb2f8d6fc9
arsenyNebozhenko <nebozhenko.arseny@gmail.com>
2020-01-25 02:14:05 +0300

    rework of hero and hero-carousel

--- src/js/main.js
+++ src/js/main.js
@@ -48,3 +48,47 @@
 
   toggleButton.addEventListener('click', toggleShowList)
 }
+
+// Hero carousel
+{
+  Siema.prototype.addPagination = function() {
+    const buttonsWrapper = document.querySelector('#hero-carousel-thumbnails')
+    const images = this.innerElements.map(item => item.querySelector('img').getAttribute('src'))
+
+    const buttons = this.innerElements.map((element, i) => {
+      const button = document.createElement('button');
+      button.className = `hero-carousel__thumbnails-item${i === 0 ? ' hero-carousel__thumbnails-item--active' : ''}`
+      button.style.backgroundImage = `url(${images[i]})`
+      
+      button.addEventListener('click', () => this.goTo(i))
+      
+      return button
+    })
+    
+    buttons.forEach(button => {
+      buttonsWrapper.appendChild(button)
+    })
+  }
+
+  Siema.prototype.activateDots = function(buttons, index) {
+    Array.from(buttons).forEach(button => button.classList.remove('hero-carousel__thumbnails-item--active'))
+    buttons[index].classList.add('hero-carousel__thumbnails-item--active')
+  }
+
+  const carousel = new Siema({
+    selector: '#hero-carousel-list',
+    loop: true,
+    onChange: function() {
+      const buttons = document.querySelector('#hero-carousel-thumbnails').children
+      this.activateDots(buttons, this.currentSlide)
+    }
+  })
+
+  const prevButton = document.querySelector('#hero-carousel-button-prev')
+  const nextButton = document.querySelector('#hero-carousel-button-next')
+
+  prevButton.addEventListener('click', () => carousel.prev())
+  nextButton.addEventListener('click', () => carousel.next())
+
+  carousel.addPagination()
+}
